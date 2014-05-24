ab5e52d97bb1561ed70302b7825efea413620e7a
Jakub Bartecek <jbartece@redhat.com>
2014-05-24 11:51:43 +0200

    Skript pocita i prumer a smerodatnou odchylku

--- thesis/perfTests/countStatistics.py
+++ thesis/perfTests/countStatistics.py
@@ -3,46 +3,124 @@ import sys
 import re
 import math
 
-def checkDifference(prev, act):
-    if prev == -1:
-        return False
-        
 
-    diff = math.fabs(prev - act)
-    relDiff = diff / prev
-    if relDiff < 0.001 and diff < 1 and act < 3000:
+class StatisticWorker:
+    checkMaxResponseTime = False
+    listLen = 3
+    
+    def __init__(self, checkMaxResponseTime):
+        self.checkMaxResponseTime = checkMaxResponseTime
+        
+    def checkDifference(self, l, act):
+        if len(l)  != self.listLen:
+            return False
+            
+        for prevTime in l:
+            if not self.checkSingleDiff(prevTime, act):
+                return False
+
         return True
-    else:
-        return False
+            
+    def checkSingleDiff(self, prev, act):        
+        diff = math.fabs(prev - act)
+        relDiff = diff / act
+        if self.compareDiff(relDiff, act):
+            return True
+        else:
+            return False
+
+    def compareDiff(self, relDiff, act):
+        if self.checkMaxResponseTime:
+            if relDiff < 0.005 and act < 4000:
+                return True
+            else:
+                return False
+        else:
+            if relDiff < 0.005:
+                return True
+            else:
+                return False
+
+    def addItem(self, l, time):
+        if len(l) < self.listLen:
+            return l + [time]
+        else:
+            return l[1:] + [time]
+
+    def run(self):
+        pattern = re.compile(r"(.+);.+;(.+);.*")
+        firstIter = True
+        stable = False
+        actList = []
+        stableValuesList = []
+
+        for line in sys.stdin:
+            if firstIter:
+                firstIter = False
+                continue
+
+            matcher = pattern.match(line)
+
+            if matcher:
+                timePast = matcher.group(1)
+                timeStr = matcher.group(2)
+                if timeStr == "null":
+                    continue
+                time = float(timeStr)
+
+                if stable:
+                    stableValuesList.append(time)
+
+                else:    
+                    if self.checkDifference(actList, time):
+                        stable = True
+                        stableTime = timePast
+                        #print("Stable time: ", timePast)
+
+                    actList = self.addItem(actList, time)
+
+            else:
+                print("Match failed! Line ", line)
+
+        arithAvg = self.countArithAverage(stableValuesList)
+        stdDeviation = self.countStdDeviation(stableValuesList, arithAvg)
+
+        self.reportResults(stableTime, arithAvg, stdDeviation)
+
+        #print("Response time average: ", arithAvg)
+
+    def countStdDeviation(self, stableValuesList, arithAvg):
+        
+        sum = 0
+        for singleValue in stableValuesList:
+            sum += (singleValue - arithAvg)**2
+
+        return math.sqrt(sum /len(stableValuesList)) 
+
+    def countArithAverage(self, l):
+        length = len(l)
+        if length == 0:
+            return "No values"
+            
+        sum = 0
+        for singleValue in l:
+            sum += singleValue
+
+        return sum / len(l)
+
+    def reportResults(self, stableTime, arithAvg, stdDeviation):
+        print(stableTime, ";", arithAvg, ";", stdDeviation, sep="")
+
+
 
 if __name__ == "__main__":
-    pattern = re.compile(r"(.+);.+;(.+);.*")
-    i = 0
-    prev = -1.0
+    if len(sys.argv) > 1:
+        flag = True
+    else:
+        flag = False
+    
+    worker = StatisticWorker(flag)
+    worker.run()
 
-    for line in sys.stdin:
-        if i == 0:
-            i += 1
-            continue
-
-        matcher = pattern.match(line)
-
-        if matcher:
-            timePast =matcher.group(1)
-            timeStr = matcher.group(2)
-            if timeStr == "null":
-                continue
-            time = float(timeStr)
-
-            if checkDifference(prev, time):
-                print("Ustaleno. Cas: ", timePast)
-                break
-
-            prev = time    
-            #print("Time: ", time)
-        else:
-            print("Match failed for line ", line)
-
-
-        i += 1
+    
 
